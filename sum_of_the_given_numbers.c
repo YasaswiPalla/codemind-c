029c4c504ffed1bf3864ca19157e1fd328b38bf9
YasaswiPalla <96292517+YasaswiPalla@users.noreply.github.com>
2022-07-02 23:50:56 +0530

    #include<stdio.h> int main() { int t,i; scanf("%d",&t); for(i=0;i<t;i++) { int a,b,sum=0; scanf("%d%d",&a,&b); sum=a+b; printf("%d ",sum); } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,b,n,s;
-scanf("%d",&n);
-while(n>0)
+int t,i;
+scanf("%d",&t);
+for(i=0;i<t;i++)
 {
-scanf("%d",&a);
-scanf("%d",&b);
-s=a+b;
+int a,b,sum=0;
+scanf("%d%d",&a,&b);
+sum=a+b;
 printf("%d
-",s);
-n--;
+",sum);
 }
-return 0;
 }
